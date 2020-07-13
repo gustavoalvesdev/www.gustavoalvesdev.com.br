cfa82eaf1cfc1c20738b05cab2b1596a1d168ed4
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-07-13 14:40:57 -0300

    validating email format on the backend

--- sistema-rc/login-action.php
+++ sistema-rc/login-action.php
@@ -5,6 +5,12 @@ require_once 'functions/usuarios.php';
 if (isset($_POST['acao'])) {
 	if (!empty($_POST['email']) && !empty($_POST['senha'])) {
 		$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+
+		if (!$email) {
+			$_SESSION['flash-danger'] = 'Formato de e-mail inválido!';
+			header('Location: login.php');
+			exit;
+		}
 		$senha = filter_var($_POST['senha']);
 
 		if(logarUsuario($email, $senha)) {
